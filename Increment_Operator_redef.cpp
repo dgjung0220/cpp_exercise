8f2377dbde63dbb357c685191cf14eed4f478fd5
정동구/연구원/MC NPD실 2팀 1파트(dg.jung@lge.com) <dg.jung@lge.com>
2017-06-28 16:33:14 +0900

    increment operator redefine

---
+++ Increment_Operator_redef.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Integer
+{
+    int value;
+
+public:
+    Integer(int n = 0) : value(n){}
+
+    Integer& operator++()
+    {
+        ++value;
+        return *this;
+    }
+    Integer operator++(int)
+    {
+        Integer temp = *this;
+        ++value;
+        return temp;
+    }
+
+    void print()
+    {
+        cout << value << endl;
+    }
+};
+
+
+int main()
+{
+//    int n1;
+    Integer n2;
+
+    n2.print();
+
+    ++n2;
+    n2.print();
+
+    n2++;
+    n2.print();
+}
